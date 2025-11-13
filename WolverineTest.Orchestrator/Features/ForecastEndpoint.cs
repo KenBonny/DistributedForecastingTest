8b69710bbc47570babe2c3cfbff81526518a3a39
Ken Bonny <github@ken.morethancode.be>
2025-11-13 11:49:32 +0100

    Integrate Wolverine for HTTP endpoints and durable messaging, add random forecast API and supporting HTTP test file.

---
+++ WolverineTest.Orchestrator/Features/ForecastEndpoint.cs
@@ -0,0 +1,13 @@
+﻿using Wolverine.Http;
+
+namespace WolverineTest.Orchestrator.Features;
+
+public class ForecastEndpoint
+{
+    [WolverinePost("/forecast/random")]
+    public static IResult Random(int? numberOfForecasts)
+    {
+        return Results.Ok(numberOfForecasts ?? 10);
+    }
+}
+
--- WolverineTest.Orchestrator/Program.cs
+++ WolverineTest.Orchestrator/Program.cs
@@ -1,10 +1,17 @@
+using Wolverine;
+using Wolverine.Http;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddWolverine(options =>
+    {
+        options.DurableScheduledMessagesLocalQueue.TelemetryEnabled(true);
+    })
+    .AddWolverineHttp();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+builder.Services.AddOpenApi().AddAuthorization();
 
 var app = builder.Build();
 
@@ -18,6 +25,6 @@ app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
-app.MapControllers();
+app.MapWolverineEndpoints();
 
 app.Run();
