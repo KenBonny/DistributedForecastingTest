cff10b171172ea49261e3febe4d7cb13016962a5
Ken Bonny <github@ken.morethancode.be>
2025-11-13 13:17:25 +0100

    Add deterministic forecast API and extend random forecast logic
    - Introduce `ForecastType` enum and `StartForecast` record.
    - Enhance random forecast endpoint to return forecast types.
    - Add deterministic forecast endpoint with a predefined sequence.
    - Update HTTP test file to cover new scenarios.

--- WolverineTest.Orchestrator/Features/ForecastEndpoint.cs
+++ WolverineTest.Orchestrator/Features/ForecastEndpoint.cs
@@ -5,9 +5,47 @@ namespace WolverineTest.Orchestrator.Features;
 public class ForecastEndpoint
 {
     [WolverinePost("/forecast/random")]
-    public static IResult Random(int? numberOfForecasts)
+    public static (IResult, IEnumerable<StartForecast>) Random(int? numberOfForecasts)
     {
-        return Results.Ok(numberOfForecasts ?? 10);
+        var amount = numberOfForecasts ?? 10;
+        var startForecasts = Enumerable.Range(0, amount)
+            .Select(_ => GetForecastType())
+            .Select(type => new StartForecast(type))
+            .ToList();
+        return (Results.Ok(startForecasts), startForecasts);
+    }
+
+    private static ForecastType GetForecastType() =>
+        System.Random.Shared.Next(100) switch
+        {
+            < 60 => ForecastType.Daily,
+            < 90 => ForecastType.Weekly,
+            _ => ForecastType.Monthly
+        };
+    
+    [WolverinePost("/forecast/deterministic")]
+    public static (IResult, IEnumerable<StartForecast>) Deterministic(int? numberOfForecasts)
+    {
+        var amount = numberOfForecasts ?? 10;
+        var startForecasts = ForecastTypes().Take(amount).Select(type => new StartForecast(type)).ToList();
+        return (Results.Ok(startForecasts), startForecasts);
+    }
+
+    private static IEnumerable<ForecastType> ForecastTypes()
+    {
+        while (true)
+        {
+            yield return ForecastType.Weekly;
+            yield return ForecastType.Daily;
+            yield return ForecastType.Weekly;
+            yield return ForecastType.Daily;
+            yield return ForecastType.Daily;
+            yield return ForecastType.Daily;
+            yield return ForecastType.Monthly;
+            yield return ForecastType.Daily;
+            yield return ForecastType.Weekly;
+            yield return ForecastType.Daily;
+        }
     }
 }
 
---
+++ WolverineTest.Orchestrator/Features/ForecastType.cs
@@ -0,0 +1,10 @@
+﻿namespace WolverineTest.Orchestrator.Features;
+
+public enum ForecastType
+{
+    Daily = 0,
+    Weekly = 1,
+    Monthly = 2
+}
+
+public record StartForecast(ForecastType Type);
