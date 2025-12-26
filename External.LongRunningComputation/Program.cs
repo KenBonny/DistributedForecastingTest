1ee91bae5d189a9b6a62db63e1eb906ad842d3a1
Ken Bonny <github@ken.morethancode.be>
2025-12-26 11:00:12 +0100

    Add `External.LongRunningComputation` project for simulating long-running operations and integrate it into the solution

---
+++ External.LongRunningComputation/Program.cs
@@ -0,0 +1,27 @@
+﻿
+var durationParseSuccess = TimeSpan.TryParse(args[0], out var duration);
+
+if (!durationParseSuccess)
+{
+    Console.WriteLine("Invalid duration format. Please provide a valid time span string.");
+    Environment.ExitCode = -1;
+    return;
+}
+
+Thread.Sleep(duration);
+
+switch (Random.Shared.Next(100))
+{
+    case < 10:
+        Console.WriteLine("Unrecoverable error detected");
+        Environment.ExitCode = -2;
+        return;
+    case < 20:
+        Console.WriteLine("Recoverable error detected");
+        Environment.ExitCode = -3;
+        return;
+    default:
+        Console.WriteLine($"Operation completed successfully after {duration}");
+        Environment.ExitCode = 0;
+        return;
+}
